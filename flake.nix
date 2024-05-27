b59d7ac9b12ced8b1e07953388b0fa4156072d64
Tobias Happ <tobias.happ@gmx.de>
2024-05-27 19:36:56 +0200

    chore: add nix setup

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "chattengauer-gudensberg";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs_latest
+          pkgs.nodePackages_latest.pnpm
+        ];
+      };
+    };
+}
