1ad4a50ae40ea7f29cf4beb1827aacabc8bc70a9
Tobias Happ <tobias.happ@gmx.de>
2024-05-27 21:27:50 +0200

    feat: add H2

--- src/lib/simpleLayout/H2.svelte
+++ src/lib/simpleLayout/H2.svelte
@@ -1,7 +1,7 @@
 <header class="flex flex-col px-4 pt-4">
-	<h1 class="text-xl font-semibold uppercase text-slate-800">
+	<h2 class="text-lg font-semibold uppercase text-slate-800">
 		<slot />
 		<span class="ml-1 inline-block h-2 w-2 rounded-sm bg-red-500" />
-	</h1>
+	</h2>
 	<div class="my-1 w-1/3 border-b-2 border-red-500" />
 </header>
--- src/lib/simpleLayout/index.ts
+++ src/lib/simpleLayout/index.ts
@@ -1,9 +1,11 @@
 import H1 from './H1.svelte';
+import H2 from './H2.svelte';
 import Root from './Root.svelte';
 import Section from './Section.svelte';
 
 export const SimpleLayout = {
 	H1,
+	H2,
 	Root,
 	Section,
 };
