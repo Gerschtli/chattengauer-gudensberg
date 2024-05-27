a8578d3929afe6f0119f265aafb0050c76131467
Tobias Happ <tobias.happ@gmx.de>
2024-05-27 21:41:20 +0200

    feat: limit width of content

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,49 +2,51 @@
 	import { Mail, Phone, User } from 'lucide-svelte';
 </script>
 
-<footer class="space-y-6 bg-slate-800 px-4 py-6 text-white">
-	<article>
-		<header>
-			<h1 class="text-sm font-bold uppercase">
-				Kontakt <span class="ml-1 inline-block h-2 w-2 rounded-sm bg-red-500" />
-			</h1>
-			<div class="my-1 w-1/4 border-b-2 border-red-500" />
-		</header>
+<footer class="bg-slate-800">
+	<div class="container grid gap-y-6 px-4 py-6 text-white md:grid-cols-2 md:gap-x-4">
+		<article>
+			<header>
+				<h1 class="text-sm font-bold uppercase">
+					Kontakt <span class="ml-1 inline-block h-2 w-2 rounded-sm bg-red-500" />
+				</h1>
+				<div class="my-1 w-1/4 border-b-2 border-red-500" />
+			</header>
 
-		<p class="mt-2 space-y-1 text-xs">
-			<span class="block">
-				<User class="mr-1 inline-block" size={16} />
-				Martin Salfer
-			</span>
-			<a class="block" href="mailto:original.chattengauer@gmx.de">
-				<Mail class="mr-1 inline-block" size={16} />
-				original.chattengauer@gmx.de
-			</a>
-			<a class="block" href="tel:+4915152763235">
-				<Phone class="mr-1 inline-block" size={16} />
-				0151 52763235
-			</a>
-		</p>
-	</article>
+			<p class="mt-2 space-y-1 text-xs">
+				<span class="block">
+					<User class="mr-1 inline-block" size={16} />
+					Martin Salfer
+				</span>
+				<a class="block" href="mailto:original.chattengauer@gmx.de">
+					<Mail class="mr-1 inline-block" size={16} />
+					original.chattengauer@gmx.de
+				</a>
+				<a class="block" href="tel:+4915152763235">
+					<Phone class="mr-1 inline-block" size={16} />
+					0151 52763235
+				</a>
+			</p>
+		</article>
 
-	<article>
-		<header>
-			<h1 class="text-sm font-bold uppercase">
-				Bankverbindung <span class="ml-1 inline-block h-2 w-2 rounded-sm bg-red-500" />
-			</h1>
-			<div class="my-1 w-1/4 border-b-2 border-red-500" />
-		</header>
+		<article>
+			<header>
+				<h1 class="text-sm font-bold uppercase">
+					Bankverbindung <span class="ml-1 inline-block h-2 w-2 rounded-sm bg-red-500" />
+				</h1>
+				<div class="my-1 w-1/4 border-b-2 border-red-500" />
+			</header>
 
-		<div class="mt-2 inline-grid grid-cols-[auto_auto] gap-x-3 gap-y-1 text-xs">
-			<div>KSK Schwalm Eder:</div>
-			<div>DE50 5205 2154 1139 3241 05</div>
-			<div>VR PartnerBank eG:</div>
-			<div>DE50 5206 2601 0000 6477 64</div>
-		</div>
-	</article>
+			<div class="mt-2 inline-grid grid-cols-[auto_auto] gap-x-3 gap-y-1 text-xs">
+				<div>KSK Schwalm Eder:</div>
+				<div>DE50 5205 2154 1139 3241 05</div>
+				<div>VR PartnerBank eG:</div>
+				<div>DE50 5206 2601 0000 6477 64</div>
+			</div>
+		</article>
 
-	<ul class="flex gap-4">
-		<li><a class="text-xs font-bold" href="/datenschutz">Datenschutz</a></li>
-		<li><a class="text-xs font-bold" href="/impressum">Impressum</a></li>
-	</ul>
+		<ul class="flex gap-4 md:col-span-2">
+			<li><a class="text-xs font-bold" href="/datenschutz">Datenschutz</a></li>
+			<li><a class="text-xs font-bold" href="/impressum">Impressum</a></li>
+		</ul>
+	</div>
 </footer>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
 
 	theme: {
 		extend: {},
+		container: {
+			center: true,
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+			},
+		},
 	},
 
 	plugins: [require('@tailwindcss/typography')],
