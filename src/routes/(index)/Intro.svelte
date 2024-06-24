ab9745a2dee0c8388d6883a4e3ad02e97ad19fe5
Tobias Happ <tobias.happ@gmx.de>
2024-06-24 22:30:14 +0200

    fix: remove whitespace before period

--- src/routes/(index)/Intro.svelte
+++ src/routes/(index)/Intro.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <SimpleLayout.Section>
+	<!-- prettier-ignore -->
 	<p class="px-4 text-sm">
 		Du spielst ein Instrument oder möchtest es lernen? Bei uns bist du richtig aufgehoben mit unseren verschiedenen
 		Orchestern: Eine
 		<a class="text-blue-700" href="/blaeserklasse">Bläserklasse</a>
 		für unsere jüngsten Mitglieder und das
-		<a class="text-blue-700" href="/orchester">Hauptorchester</a>
-		.
+		<a class="text-blue-700" href="/orchester">Hauptorchester</a>.
 	</p>
 </SimpleLayout.Section>
