be17b2069d71f2bd080952a401e9a2ec23154e0a
Tobias Happ <tobias.happ@gmx.de>
2024-12-12 23:28:18 +0100

    chore: use static import for Snippet type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
+
 	import '../app.css';
 
 	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
 
 	interface Props {
-		children?: import('svelte').Snippet;
+		children?: Snippet;
 	}
 
 	let { children }: Props = $props();
