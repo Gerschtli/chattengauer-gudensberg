e65c55c19facc44caadc275b0a2a53371a1f5652
Tobias Happ <tobias.happ@gmx.de>
2024-12-15 13:12:51 +0100

    feat: add claim in header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Menu } from 'lucide-svelte';
+	import { MenuIcon } from 'lucide-svelte';
 
 	import schrift from '$lib/assets/schrift.svg';
 	import wave from '$lib/assets/wave.svg';
@@ -9,15 +9,21 @@
 
 <svelte:window bind:scrollY />
 
-<header class="sticky top-0 z-10 bg-white/95 shadow-header">
-	<div class="container flex w-full items-center justify-between transition-all" class:py-4={scrollY === 0}>
-		<a href="/" class="flex items-center gap-4 px-4">
-			<img src={schrift} class="h-10" alt="Logo der Chattengauer Gudensberg" />
-			<img src={wave} class="h-14" alt="Logo der Chattengauer Gudensberg" />
-		</a>
+<header class="sticky top-0 z-10 bg-white/95 shadow-header py-1">
+	<div class="container flex justify-between w-full px-4 transition-all" class:py-3={scrollY === 0}>
+		<div class="flex flex-col sm:items-end sm:gap-8 sm:flex-row gap-2">
+			<a href="/" class="flex items-center gap-4">
+				<img src={schrift} class="h-10" alt="Logo der Chattengauer Gudensberg" />
+				<img src={wave} class="h-14" alt="Logo der Chattengauer Gudensberg" />
+			</a>
+			<!-- TODO: fix jumpy behaviour when scrolling -->
+			<p class="font-bold text-sm text-slate-600 sm:pb-2 sm:block italic" class:hidden={scrollY > 0}>
+				Dein Musikverein im Chattengau!
+			</p>
+		</div>
 
 		<div class="p-4">
-			<Menu size={32} />
+			<MenuIcon size={32} />
 		</div>
 	</div>
 </header>
