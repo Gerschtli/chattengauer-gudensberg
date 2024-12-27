2de7eafadfab64a8d217447dfffe82df104300f2
Tobias Happ <tobias.happ@gmx.de>
2024-12-27 03:33:07 +0100

    feat: add static pages

---
+++ src/routes/probenraum/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { SimpleLayout } from '$lib/simpleLayout';
+</script>
+
+<SimpleLayout.Root title="Probenraum">
+	<SimpleLayout.Section class="space-y-4">
+		<SimpleLayout.H1>Probenraum</SimpleLayout.H1>
+
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis doloribus provident dolore, molestiae aut
+			nobis obcaecati quaerat aliquid nemo ratione?
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laudantium iste numquam ducimus distinctio
+			omnis similique dignissimos, rerum facilis repellendus nesciunt minima possimus autem nemo sit dolorum est
+			quibusdam. Cumque laborum molestiae voluptatem officiis. Provident nostrum quis recusandae reiciendis optio
+			facilis voluptatibus tenetur quisquam vitae eligendi? Beatae harum quas quis.
+		</p>
+	</SimpleLayout.Section>
+</SimpleLayout.Root>
