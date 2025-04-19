1801f3d8e94ac0089bbafc09b3a8a4e7431721de
Tobias Happ <tobias.happ@gmx.de>
2025-04-19 02:39:26 +0200

    chore: remove unused properties

--- src/lib/ensembles.ts
+++ src/lib/ensembles.ts
@@ -7,18 +7,14 @@ export function isEnsemble(slug: string): slug is Ensemble {
 export const ensembles = {
 	'grosses-orchester': {
 		name: 'Großes Orchester',
-		claim: 'Leidenschaft trifft Klang',
 	},
 	blaeserklassen: {
 		name: 'Bläserklassen Kid.Chatts',
-		claim: 'Der perfekte Einstieg',
 	},
 	jugendorchester: {
 		name: 'Jugendorchester',
-		claim: 'Energiegeladene Sounds',
 	},
 	'brass-und-spass': {
 		name: 'Brass & Spaß',
-		claim: 'Dein musikalischer Neuanfang',
 	},
 };
