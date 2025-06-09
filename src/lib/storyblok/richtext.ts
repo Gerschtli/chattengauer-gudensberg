9b448bd9fd54b0dc375540bdeab45a75cb15e779
Tobias Happ <tobias.happ@gmx.de>
2025-06-09 16:02:58 +0200

    fix: fix rendering of heading in richtext

--- src/lib/storyblok/richtext.ts
+++ src/lib/storyblok/richtext.ts
@@ -6,8 +6,9 @@ const richText = richTextResolver({
 	resolvers: {
 		[BlockTypes.HEADING]: (node) => {
 			const level = node.attrs?.['level'];
+			const children = Array.isArray(node.children) ? node.children.map((c) => c.trim()).join('') : node.children;
 
-			return `<h${level} class="heading-${level}"><span>${node.children}</span>&nbsp;<span></span></h${level}>`;
+			return `<h${level} class="heading-${level}"><span>${children}</span>&nbsp;<span></span></h${level}>`;
 		},
 		[MarkTypes.LINK]: (node) => {
 			const href = node.attrs?.['href'];
