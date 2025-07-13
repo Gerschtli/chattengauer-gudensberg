c50c40dd7517378873ac4bfe359c07b17167226b
Tobias Happ <tobias.happ@gmx.de>
2025-07-13 16:38:32 +0200

    chore: remove unused comment

--- src/lib/components/richtext/form/Potty.svelte
+++ src/lib/components/richtext/form/Potty.svelte
@@ -1,6 +1,5 @@
 <div class="absolute flex flex-col gap-y-1" aria-hidden="true">
 	<label class="text-sm" for="formsnap-99">&nbsp;</label>
-	<!-- svelte-ignore a11y_autocomplete_valid -->
 	<input
 		id="formsnap-99"
 		name="code"
