3db7e2eceab114afe68f431b7bfb8c52dab25af3
Tobias Happ <tobias.happ@gmx.de>
2026-09-10 23:04:07 +0200

    fix: fix Events component

--- src/lib/components/richtext/events/Events.svelte
+++ src/lib/components/richtext/events/Events.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { ChevronDownIcon } from 'lucide-svelte';
-	import { SvelteSet } from 'svelte/reactivity';
-	import { slide } from 'svelte/transition';
 
 	import { storyblokEditable } from '$lib/storyblok/util';
 	import { TIME_ZONE, getStart } from '$lib/time';
@@ -10,60 +8,51 @@
 
 	import { getEnsembleList } from '../newsList/context';
 
-	import Event from './Event.svelte';
+	import { SvelteMap } from 'svelte/reactivity';
 	import { getEvents } from './context';
+	import Event from './Event.svelte';
 
 	let { blok }: { blok: Events } = $props();
 
 	const events = getEvents();
 	const ensembleMap = Object.fromEntries(getEnsembleList().map((e) => [e.url, e.name]));
 
-	const slideDuration = 500;
 	const showMax = $derived(parseInt(blok.showMax));
 	let expandAll = $state(false);
 
-	const set = new SvelteSet<string>();
+	const eventsGrouped = $derived.by(() => {
+		const map = new SvelteMap<string, EventData[]>();
+		for (const [i, event] of events.entries()) {
+			if (!expandAll && i + 1 > showMax) break;
 
-	function buildSeparator(event: EventData) {
-		const rendered = new Intl.DateTimeFormat('de-DE', {
-			month: 'long',
-			year: 'numeric',
-			timeZone: TIME_ZONE,
-		}).format(getStart(event.time));
+			const separator = new Intl.DateTimeFormat('de-DE', {
+				month: 'long',
+				year: 'numeric',
+				timeZone: TIME_ZONE,
+			}).format(getStart(event.time));
 
-		if (set.has(rendered)) return undefined;
+			const eventsMonth = map.get(separator) ?? [];
+			eventsMonth.push(event);
 
-		set.add(rendered);
+			map.set(separator, eventsMonth);
+		}
 
-		return rendered;
-	}
+		return map;
+	});
 </script>
-
-{#snippet eventWithSeparator(event: EventData)}
-	{@const separator = buildSeparator(event)}
-	{#if separator}
-		<h2 class="text-xs font-bold text-slate-700 uppercase">
-			{separator}
-		</h2>
-	{/if}
-	<Event {event} {ensembleMap} />
-{/snippet}
 
 <div class="grid gap-y-4" use:storyblokEditable={blok}>
 	{#if events.length === 0}
 		<p class="text-slate-600">{blok.emptyText}</p>
-	{/if}
-
-	{#each events.slice(0, showMax) as event, i (i)}
-		{@render eventWithSeparator(event)}
-	{/each}
-
-	{#if expandAll}
-		<div class="grid gap-y-4" in:slide={{ duration: slideDuration }}>
-			{#each events.slice(showMax) as event, i (i)}
-				{@render eventWithSeparator(event)}
+	{:else}
+		{#each eventsGrouped.entries() as [separator, eventList], i (i)}
+			<h2 class="text-xs font-bold text-slate-700 uppercase">
+				{separator}
+			</h2>
+			{#each eventList as event, j (j)}
+				<Event {event} {ensembleMap} />
 			{/each}
-		</div>
+		{/each}
 	{/if}
 
 	{#if !expandAll && events.length > showMax}
